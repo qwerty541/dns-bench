0dddd67409e51be7a461a0288c83ee576af37f25
Alexandr <qwerty541zxc@gmail.com>
2024-04-21 00:39:52 +0300

    refactor: rewrite application code to make it more structured

--- src/main.rs
+++ src/main.rs
@@ -26,140 +26,204 @@ use std::time::Duration;
 use std::time::Instant;
 use tabled::Table;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let arguments = Arguments::parse();
+pub struct DnsBenchApplication {
+    arguments: Arguments,
+    dns_entries: sync::Arc<sync::Mutex<collections::VecDeque<servers::DnsEntry>>>,
+    result_entries: sync::Arc<sync::Mutex<Vec<ResultEntry>>>,
+    threads: Vec<thread::JoinHandle<()>>,
+    progress_bar: Option<ProgressBar>,
+    bench_start_time: Option<Instant>,
+}
 
-    println!(
-        "Starting DNS benchmark with the following parameters:\n\
-        Domain: {}\n\
-        Threads: {}\n\
-        Requests: {}\n\
-        Protocol: {}\n\
-        Name servers: IP{}; Lookup: IP{}",
-        arguments.domain,
-        arguments.threads,
-        arguments.requests,
-        arguments.protocol,
-        arguments.name_servers_ip,
-        arguments.lookup_ip,
-    );
+impl DnsBenchApplication {
+    /// Create a new instance of the application.
+    pub fn new(arguments: Arguments) -> Self {
+        Self {
+            arguments,
+            dns_entries: sync::Arc::new(sync::Mutex::new(collections::VecDeque::default())),
+            result_entries: sync::Arc::new(sync::Mutex::new(Vec::new())),
+            threads: Vec::new(),
+            progress_bar: None,
+            bench_start_time: None,
+        }
+    }
 
-    // Create a progress bar with the desired style
-    let pb = ProgressBar::new(match arguments.name_servers_ip {
-        args::IpAddr::V4 => IPV4_DNS_ENTRIES.len() * arguments.requests,
-        args::IpAddr::V6 => IPV6_DNS_ENTRIES.len() * arguments.requests,
-    } as u64);
-    pb.set_style(
-        ProgressStyle::default_bar()
-            .template("{spinner:.bold.green} [{elapsed}] [{bar:80.cyan/blue}] {pos}/{len} ({eta})")?
-            .progress_chars("#>-"),
-    );
+    /// Run the application.
+    pub fn run(&mut self) {
+        self.print_arguments_summary();
+        self.init_progress_bar();
+        self.fill_dns_entries();
+        self.bench_start_time();
+        self.spawn_threads();
+        self.await_threads();
+        self.clear_progress_bar();
+        self.sort_result_entries();
+        self.print_result();
+        self.print_bench_elapsed_time();
+    }
 
-    // Create the shared DNS entries and result entries
-    let dns_entries = sync::Arc::new(sync::Mutex::new(collections::VecDeque::from(
-        match arguments.name_servers_ip {
+    /// Print the arguments summary.
+    fn print_arguments_summary(&self) {
+        println!(
+            "Starting DNS benchmark with the following parameters:\n\
+            Domain: {}\n\
+            Threads: {}\n\
+            Requests: {}\n\
+            Protocol: {}\n\
+            Name servers: IP{}; Lookup: IP{}",
+            self.arguments.domain,
+            self.arguments.threads,
+            self.arguments.requests,
+            self.arguments.protocol,
+            self.arguments.name_servers_ip,
+            self.arguments.lookup_ip,
+        );
+    }
+
+    /// Create a progress bar with the desired style.
+    fn init_progress_bar(&mut self) {
+        let pb = ProgressBar::new(match self.arguments.name_servers_ip {
+            args::IpAddr::V4 => IPV4_DNS_ENTRIES.len() * self.arguments.requests,
+            args::IpAddr::V6 => IPV6_DNS_ENTRIES.len() * self.arguments.requests,
+        } as u64);
+        pb.set_style(
+            ProgressStyle::default_bar()
+                .template(
+                    "{spinner:.bold.green} [{elapsed}] [{bar:80.cyan/blue}] {pos}/{len} ({eta})",
+                )
+                .unwrap()
+                .progress_chars("#>-"),
+        );
+        self.progress_bar = Some(pb);
+    }
+
+    /// Fill the DNS entries with the desired IP version.
+    fn fill_dns_entries(&mut self) {
+        let dns_entries = match self.arguments.name_servers_ip {
             args::IpAddr::V4 => IPV4_DNS_ENTRIES.clone(),
             args::IpAddr::V6 => IPV6_DNS_ENTRIES.clone(),
-        },
-    )));
-    let result_entries = sync::Arc::new(sync::Mutex::new(Vec::new()));
-    let mut handles = Vec::new();
-    let bench_start_time = Instant::now();
+        };
+        self.dns_entries.lock().unwrap().extend(dns_entries);
+    }
 
-    // Create the threads
-    for _ in 0..arguments.threads {
-        let dns_entries_clone = dns_entries.clone();
-        let result_entries_clone = result_entries.clone();
-        let arguments_clone = arguments.clone();
-        let pb_clone = pb.clone();
+    /// Start the benchmark timer.
+    fn bench_start_time(&mut self) {
+        self.bench_start_time = Some(Instant::now());
+    }
 
-        handles.push(thread::spawn(move || loop {
-            let dns_entry = {
-                let mut dns_entries_clone = dns_entries_clone.lock().unwrap();
-                dns_entries_clone.pop_front()
-            };
+    /// Spawn the threads.
+    fn spawn_threads(&mut self) {
+        for _ in 0..self.arguments.threads {
+            let dns_entries = self.dns_entries.clone();
+            let result_entries = self.result_entries.clone();
+            let arguments = self.arguments.clone();
+            let pb = self.progress_bar.as_ref().unwrap().clone();
 
-            if let Some(dns_entry) = dns_entry {
-                let mut measure_results = Vec::new();
+            self.threads.push(thread::spawn(move || loop {
+                let dns_entry = {
+                    let mut dns_entries = dns_entries.lock().unwrap();
+                    dns_entries.pop_front()
+                };
 
-                for _ in 0..arguments_clone.requests {
-                    // Create a new resolver for each request to avoid caching
-                    let mut resolver_config = ResolverConfig::new();
-                    resolver_config.add_name_server(NameServerConfig {
-                        socket_addr: dns_entry.socker_addr,
-                        protocol: arguments_clone.protocol.into(),
-                        tls_dns_name: None,
-                        trust_negative_responses: false,
-                        bind_addr: None,
-                    });
-                    let mut resolver_opts = ResolverOpts::default();
-                    resolver_opts.attempts = 0;
-                    resolver_opts.timeout = Duration::from_secs(arguments_clone.timeout);
-                    resolver_opts.ip_strategy = arguments_clone.lookup_ip.into();
-                    let resolver = Resolver::new(resolver_config, resolver_opts).unwrap();
+                if let Some(dns_entry) = dns_entry {
+                    let mut measure_results = Vec::new();
 
-                    // Measure the time it takes to resolve the domain
-                    let start_time = Instant::now();
-                    let result_entry: MeasureResult =
-                        match resolver.lookup_ip(arguments_clone.domain.clone()) {
-                            Ok(response) => {
-                                let elapsed_time = start_time.elapsed();
-                                MeasureResult {
+                    for _ in 0..arguments.requests {
+                        // Create a new resolver for each request to avoid caching.
+                        let mut resolver_config = ResolverConfig::new();
+                        resolver_config.add_name_server(NameServerConfig {
+                            socket_addr: dns_entry.socker_addr,
+                            protocol: arguments.protocol.into(),
+                            tls_dns_name: None,
+                            trust_negative_responses: false,
+                            bind_addr: None,
+                        });
+                        let mut resolver_opts = ResolverOpts::default();
+                        resolver_opts.attempts = 0;
+                        resolver_opts.timeout = Duration::from_secs(arguments.timeout);
+                        resolver_opts.ip_strategy = arguments.lookup_ip.into();
+                        let resolver = Resolver::new(resolver_config, resolver_opts).unwrap();
+
+                        // Measure the time it takes to resolve the domain.
+                        let start_time = Instant::now();
+                        let result_entry: MeasureResult =
+                            match resolver.lookup_ip(arguments.domain.clone()) {
+                                Ok(response) => {
+                                    let elapsed_time = start_time.elapsed();
+                                    MeasureResult {
+                                        name: dns_entry.name.clone(),
+                                        ip: dns_entry.socker_addr.ip(),
+                                        resolved_ip: response.iter().next().unwrap(),
+                                        time: TimeResult::Succeeded(elapsed_time),
+                                    }
+                                }
+                                Err(e) => MeasureResult {
                                     name: dns_entry.name.clone(),
                                     ip: dns_entry.socker_addr.ip(),
-                                    resolved_ip: response.iter().next().unwrap(),
-                                    time: TimeResult::Succeeded(elapsed_time),
-                                }
-                            }
-                            Err(e) => MeasureResult {
-                                name: dns_entry.name.clone(),
-                                ip: dns_entry.socker_addr.ip(),
-                                resolved_ip: match arguments_clone.lookup_ip {
-                                    args::IpAddr::V4 => IpAddr::V4(Ipv4Addr::UNSPECIFIED),
-                                    args::IpAddr::V6 => IpAddr::V6(Ipv6Addr::UNSPECIFIED),
+                                    resolved_ip: match arguments.lookup_ip {
+                                        args::IpAddr::V4 => IpAddr::V4(Ipv4Addr::UNSPECIFIED),
+                                        args::IpAddr::V6 => IpAddr::V6(Ipv6Addr::UNSPECIFIED),
+                                    },
+                                    time: TimeResult::Failed(e.to_string()),
                                 },
-                                time: TimeResult::Failed(e.to_string()),
-                            },
-                        };
-                    measure_results.push(result_entry);
-                    pb_clone.inc(1);
+                            };
+                        measure_results.push(result_entry);
+                        pb.inc(1);
+                    }
+                    let result_entry: ResultEntry = measure_results.into();
+                    result_entries.lock().unwrap().push(result_entry);
+                } else {
+                    break;
                 }
-                let result_entry: ResultEntry = measure_results.into();
-                result_entries_clone.lock().unwrap().push(result_entry);
-            } else {
-                break;
-            }
-        }));
+            }));
+        }
     }
 
-    // Wait for all threads to finish
-    for handle in handles {
-        handle.join().unwrap();
+    /// Wait for all threads to finish.
+    fn await_threads(&mut self) {
+        for handle in self.threads.drain(..) {
+            handle.join().unwrap();
+        }
     }
 
-    pb.finish_and_clear();
+    /// Finish and clear the progress bar.
+    fn clear_progress_bar(&mut self) {
+        self.progress_bar.as_ref().unwrap().finish_and_clear();
+    }
 
-    // Sort result entries by average duration, failed entries are at the end.
-    let mut result_entries = result_entries.lock().unwrap();
-    result_entries.sort_by(|a, b| {
-        let a = match a.average_duration {
-            TimeResult::Succeeded(duration) => duration,
-            TimeResult::Failed(_) => Duration::new(u64::MAX, 0),
-        };
-        let b = match b.average_duration {
-            TimeResult::Succeeded(duration) => duration,
-            TimeResult::Failed(_) => Duration::new(u64::MAX, 0),
-        };
-        a.cmp(&b)
-    });
+    /// Sort result entries by average duration, failed entries are at the end.
+    fn sort_result_entries(&self) {
+        let mut result_entries = self.result_entries.lock().unwrap();
+        result_entries.sort_by(|a, b| {
+            let a = match a.average_duration {
+                TimeResult::Succeeded(duration) => duration,
+                TimeResult::Failed(_) => Duration::new(u64::MAX, 0),
+            };
+            let b = match b.average_duration {
+                TimeResult::Succeeded(duration) => duration,
+                TimeResult::Failed(_) => Duration::new(u64::MAX, 0),
+            };
+            a.cmp(&b)
+        });
+    }
 
-    // Print the result
-    let table = Table::new(&*result_entries).to_string();
-    println!("{}", table);
+    /// Print the result.
+    fn print_result(&self) {
+        let table = Table::new(&*self.result_entries.lock().unwrap()).to_string();
+        println!("{}", table);
+    }
 
-    // Print the benchmark time
-    let bench_elapsed_time = bench_start_time.elapsed();
-    println!("Benchmark completed in {bench_elapsed_time:?}",);
+    /// Print the benchmark time.
+    fn print_bench_elapsed_time(&self) {
+        let bench_elapsed_time = self.bench_start_time.unwrap().elapsed();
+        println!("Benchmark completed in {bench_elapsed_time:?}",);
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let arguments = Arguments::parse();
+    let mut app = DnsBenchApplication::new(arguments);
+    app.run();
 
     Ok(())
 }
