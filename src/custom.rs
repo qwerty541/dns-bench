5dc674324801b40ea78073e7e5c7bed87fab7f5e
Alexandr <qwerty541zxc@gmail.com>
2024-07-28 21:49:02 +0300

    tests: fix custom IPv4 servers file parse test

--- src/custom.rs
+++ src/custom.rs
@@ -68,7 +68,7 @@ mod tests {
         let filepath = PathBuf::from("./examples/ipv4-custom-servers-example.txt");
         let entries = read_custom_servers_list(filepath, IpAddr::V4).unwrap();
 
-        assert_eq!(entries.len(), 25);
+        assert_eq!(entries.len(), 27);
         assert_eq!(entries[0].name, "Google");
         assert_eq!(entries[0].socket_addr, "8.8.8.8:53".parse().unwrap());
     }
