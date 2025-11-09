57deef50c8fbcd2d416a55a0743694d48c7dc70d
Alexandr Garbuzov <qwerty541zxc@gmail.com>
2025-11-09 21:59:19 +0200

    build: add target installation command to the windows exe build script

--- build-win-exe.sh
+++ build-win-exe.sh
@@ -2,4 +2,7 @@
 
 set -e
 
+# Keep this line for situations where the default toolchain was changed, so the target needs to be re-added
+rustup target add x86_64-pc-windows-gnu
+
 cargo build --release --target x86_64-pc-windows-gnu
