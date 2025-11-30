68d96be01a3f14c7b21ed40141c17665431b1c44
Alexandr Garbuzov <qwerty541zxc@gmail.com>
2025-11-30 13:26:09 +0200

    chore: set 25 requests inside docker test scripts

--- docker/test-ci.sh
+++ docker/test-ci.sh
@@ -1,1 +1,1 @@
-docker run --rm --name dns-bench --network host dns-bench-test:latest --timeout 1 --threads 16
+docker run --rm --name dns-bench --network host dns-bench-test:latest --timeout 1 --threads 16 --requests 25
